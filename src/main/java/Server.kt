c6f68d707c039a06053d8b8d418b50791185ab7b
pacman29 <zx19-95@mail.ru>
2017-10-10 17:33:24 +0300

    add cached thread pool with

--- src/main/java/Server.kt
+++ src/main/java/Server.kt
@@ -1,18 +1,22 @@
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.Unconfined
 import kotlinx.coroutines.experimental.*
+import java.lang.management.ManagementFactory
 import java.net.ServerSocket
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
+import java.util.concurrent.*
 
 class Server {
     companion object {
         fun start(port: Int, root: String) {
             val cpus = Runtime.getRuntime().availableProcessors()
-            val cd = Executors.newCachedThreadPool().asCoroutineDispatcher()
+            val myCachedThreadPool = ThreadPoolExecutor(cpus, cpus,
+                    60L, TimeUnit.SECONDS,
+                    SynchronousQueue());
+            val cd = myCachedThreadPool.asCoroutineDispatcher()
 
             val socket = ServerSocket(port)
             println("Server started on port $port")
+            println("Pid: ${ManagementFactory.getRuntimeMXBean().getName()}")
             while (true) {
                 val accept = socket.accept()
                 launch(cd) {
